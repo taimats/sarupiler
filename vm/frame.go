3c31a12986f7cbf3a6c04f40d765c0f4feabafbe
taimats <beo8939@gmail.com>
2025-07-26 17:26:40 +0900

    add: frame file

---
+++ vm/frame.go
@@ -0,0 +1,23 @@
+package vm
+
+import (
+	"github.com/taimats/sarupiler/code"
+	obj "github.com/taimats/sarupiler/object"
+)
+
+const (
+	MaxFrames = 1024
+)
+
+type Frame struct {
+	fn *obj.CompiledFunction
+	ip int //instruction pointer
+}
+
+func NewFrame(fn *obj.CompiledFunction) *Frame {
+	return &Frame{fn: fn, ip: -1}
+}
+
+func (f *Frame) Instructions() code.Instructions {
+	return f.fn.Instructions
+}
